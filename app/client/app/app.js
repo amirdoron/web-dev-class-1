3f0e3318bfddcdf5365f5a5611cd7c488112991b
amir <amiram279@.012.net.il>
2015-02-16 14:23:52 +0200

    add some comments related to angular

--- app/client/app/app.js
+++ app/client/app/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
+// Declare app level module which depends on views, and components.
+// the module name in this case is myApp, that depends on a list of additional dependencies.
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
+// 
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
