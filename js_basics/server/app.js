b9e47b5c2445892bee7044ce46c67ac076f2dd19
amir <amiram279@.012.net.il>
2015-02-17 17:01:48 +0200

    pack node's server artifcats in new server directory (along side the client directory)

--- js_basics/server/app.js
+++ js_basics/server/app.js
@@ -11,12 +11,12 @@ function InitServer()
 	var app = express();
 
 	// instruct the server to serve client's static files from ./client directory
-	app.use(express.static(__dirname + '/client'));
+	app.use(express.static(__dirname + '/../client'));
 	app.use(bodyParser.json());
 
 	// starting the server
 	app.listen(8000);
-	console.log("server listen on port 8000")
+	console.log("server listen on port 8000");
 
 	return app;
 }
