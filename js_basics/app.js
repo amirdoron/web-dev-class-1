4f68fa2f2a8948b8f6d788dc27b8bf6564d6336a
amir <amiram279@.012.net.il>
2015-02-17 13:30:52 +0200

    move person logic into personService and expose CreateRoutes() to construct person REST API

--- js_basics/app.js
+++ js_basics/app.js
@@ -1,39 +1,6 @@
 
 console.log("hello world");
 
-// Alternative for the above.
-// Class like example.
-// A Person type that requires 3 parameters during creation
-// This is a function (not a Person object yet).
-// You'll need to use new Person(...) for creating a Person instance.
-function Person(id, name, gender)
-{
-	// Person's data members
-	this.mId = id;
-	this.mName = name;
-	this.mGender = gender;
-
-	// Person's method
-  this.ToString = function ()
-  {
-    return "Person ID " + this.mId + " Name " + this.mName + " Gender " + this.mGender;
-  }
-}
-
-function CreatePersons()
-{
-	var persons =	[
-			new Person(1, "person1", "Male"),
-			new Person(2, "person2", "Female"),
-			new Person(3, "person3", "Male")
-	];
-
-	return persons;
-}
-
-// Simulate PersonDAL object
-var personDB = CreatePersons();
-
 function InitServer()
 {
 	// load express external lib
@@ -41,31 +8,6 @@ function InitServer()
 
 	// construct an express object
 	var app = express();
-
-	// define a REST route for getting a Person
-	// to access this route you can call from web-browser to http://localhost:8000/person/get
-	app.get('/person/get/:id', function (req, res)
-	{
-		// create a list of Persons with ID that match the requested id
-		// consider moving such logic to an object that represents PersonDB e.g. PersonDAL
-		var foundPersons = personDB.filter(function(p){
-			return p.mId == req.params.id;
-		});
-
-		if(foundPersons[0] != null)
-		{
-			// use Person method
-			console.log("sending " + foundPersons[0].ToString());
-
-			// sending a javascript object is automatically serialized to a JSON
-			res.send(foundPersons[0]);
-		}
-		else
-		{
-			res.send("Person " + req.params.id + " not found");
-		}
-
-	});
 
 	// starting the server
 	var server = app.listen(8000, function () {
@@ -75,7 +17,10 @@ function InitServer()
 
 		console.log('Example app listening at http://%s:%s', host, port);
 	});
+
+	return app;
 }
 
-InitServer();
-
+var app = InitServer();
+var personService = require('./personService.js');
+var service = personService(app);
---
+++ js_basics/personService.js
@@ -0,0 +1,77 @@
+// expose module as a object
+// User should instantiate with a parameter (similar to ctor)
+module.exports = function(app)
+{
+	// Alternative for the above.
+	// Class like example.
+	// A Person type that requires 3 parameters during creation
+	// This is a function (not a Person object yet).
+	// You'll need to use new Person(...) for creating a Person instance.
+	function Person(id, name, gender)
+	{
+		// Person's data members
+		this.mId = id;
+		this.mName = name;
+		this.mGender = gender;
+
+		// Person's method
+		this.ToString = function ()
+		{
+			return "Person ID " + this.mId + " Name " + this.mName + " Gender " + this.mGender;
+		}
+	}
+
+	function CreatePersons()
+	{
+		var persons =	[
+			new Person(1, "person1", "Male"),
+			new Person(2, "person2", "Female"),
+			new Person(3, "person3", "Male")
+		];
+
+		return persons;
+	}
+
+	// Simulate PersonDAL object
+	var personDB = CreatePersons();
+
+	// define person REST API
+	// define a REST route for getting a Person
+	// to access this route you can call from web-browser to http://localhost:8000/person/get
+	app.get('/person/get/:id', function (req, res)
+	{
+		// create a list of Persons with ID that match the requested id
+		// consider moving such logic to an object that represents PersonDB e.g. PersonDAL
+		var foundPersons = personDB.filter(function(p){
+			return p.mId == req.params.id;
+		});
+
+		if(foundPersons[0] != null)
+		{
+			// use Person method
+			console.log("sending " + foundPersons[0].ToString());
+
+			// sending a javascript object is automatically serialized to a JSON
+			res.send(foundPersons[0]);
+		}
+		else
+		{
+			res.send("Person " + req.params.id + " not found");
+		}
+	});
+
+	// return an object that contains the public API related to this service
+	return {
+		// Each method may access other data members and functions that are
+		// defined in this closure (you may consider everything that is external
+		// these methods as private elements of this closure i.e. module
+		SomeMethod1: function ()
+		{
+
+		},
+		SomeMethod2: function ()
+		{
+
+		}
+	};
+}
