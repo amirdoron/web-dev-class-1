7520508200c1efff1032e9199f379533c905f462
amir <amiram279@.012.net.il>
2015-02-17 14:59:23 +0200

    delete unused express folder

--- js_basics/app.js
+++ js_basics/app.js
@@ -5,9 +5,12 @@ function InitServer()
 {
 	// load express external lib
 	var express = require('express');
-
 	// construct an express object
 	var app = express();
+
+	// serve client static files
+	//var serveStatic = require('serve-static');
+	//app.use(serveStatic('../app/client/app/', {'index': ['default.html', 'default.htm']}));
 
 	// starting the server
 	var server = app.listen(8000, function () {
@@ -22,5 +25,6 @@ function InitServer()
 }
 
 var app = InitServer();
+
 var personService = require('./personService.js');
 var service = personService(app);
