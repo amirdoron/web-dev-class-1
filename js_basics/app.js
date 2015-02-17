5866715f789083425d41e96053dd9e06995742c5
amir <amiram279@.012.net.il>
2015-02-17 13:12:02 +0200

    demonstrate express - change person/get route to accept Person's ID and query persons according to their ID.

--- js_basics/app.js
+++ js_basics/app.js
@@ -20,31 +20,62 @@ function Person(id, name, gender)
   }
 }
 
-// load express external lib
-var express = require('express');
-
-// construct an express object
-var app = express();
-
-// define a REST route for getting a Person
-// to access this route you can call from web-browser to http://localhost:8000/person/get
-app.get('/person/get', function (req, res)
+function CreatePersons()
 {
-	// instantiate a Person object
-	var rogerRabitPerson = new Person(1234, "Roger Rabit", "Male");
+	var persons =	[
+			new Person(1, "person1", "Male"),
+			new Person(2, "person2", "Female"),
+			new Person(3, "person3", "Male")
+	];
 
-	// use Person method
-	console.log("sending " + rogerRabitPerson.ToString());
+	return persons;
+}
 
-	// sending a javascript object is automatically serialized to a JSON
-	res.send(rogerRabitPerson);
-});
+// Simulate PersonDAL object
+var personDB = CreatePersons();
 
-// starting the server
-var server = app.listen(8000, function () {
+function InitServer()
+{
+	// load express external lib
+	var express = require('express');
 
-	var host = server.address().address;
-	var port = server.address().port;
+	// construct an express object
+	var app = express();
 
-	console.log('Example app listening at http://%s:%s', host, port);
-});
+	// define a REST route for getting a Person
+	// to access this route you can call from web-browser to http://localhost:8000/person/get
+	app.get('/person/get/:id', function (req, res)
+	{
+		// create a list of Persons with ID that match the requested id
+		// consider moving such logic to an object that represents PersonDB e.g. PersonDAL
+		var foundPersons = personDB.filter(function(p){
+			return p.mId == req.params.id;
+		});
+
+		if(foundPersons[0] != null)
+		{
+			// use Person method
+			console.log("sending " + foundPersons[0].ToString());
+
+			// sending a javascript object is automatically serialized to a JSON
+			res.send(foundPersons[0]);
+		}
+		else
+		{
+			res.send("Person " + req.params.id + " not found");
+		}
+
+	});
+
+	// starting the server
+	var server = app.listen(8000, function () {
+
+		var host = server.address().address;
+		var port = server.address().port;
+
+		console.log('Example app listening at http://%s:%s', host, port);
+	});
+}
+
+InitServer();
+
