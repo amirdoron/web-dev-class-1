d4b38cce568183536985326c3cda84153152db37
amir <amiram279@.012.net.il>
2015-02-22 09:45:08 +0200

    how should we display persons on HTML?

--- js_basics/client/person.js
+++ js_basics/client/person.js
@@ -1,32 +1,16 @@
-// Self executed anonymous function
-// This is a function that is being executed once when the js module is loaded.
-// It hides inside its internal stuff, and may expose subset interface to the outer javascript world.
-// The code template for writing self exe anonymous function is as the following:
-// (function()
-// {
-			// your code goes here
-// })();
-
 (function(globalSpace, ObjectDm, $) // ObjectDm is know become an external dependency
 {
 	"use strict";
 
-	// expose public API by injecting new object (PersonService) to the global namespace.
-	var exportPersonService = {}; // define new object to contain the public API
-	globalSpace.PersonService = exportPersonService; // insert new namespace in the global one
+	var exportPersonService = {};
+	globalSpace.PersonService = exportPersonService;
 
-	// create new person DM to bind between HTML view and this javascript controller
 	var personDm = new ObjectDm( "person" );
-	// set some initial value for the person ID (up to this point the default value was set on the HTML)
 	personDm.set("id", 1);
 
 	exportPersonService.GetById = function()
 	{
-		// To cancel direct dependency between this controller and HTML tag's ID defined in the view
-		// to allow fetching the person ID, we can use MVC instead ...
-		// so instead of doing: var id = $('#personId').val();
-		// we can do:
-		var id = personDm.get("id"); // where id represents the person's id property
+		var id = personDm.get("id");
 
 		$.ajax({
 			url: "/person/get/"+id,
@@ -37,10 +21,6 @@
 					" name " + person.Name +
 					" Gender " + person.Gender;
 
-				// Set person data depends on specific HTML tag's ID
-				// MVC pattern is also suitable for this case as well ...
-				//so instead of doing: $('#person-result').html(personResult);
-				// we can do:
 				personDm.set( "result", personResult ); // where result represents person's data result
 			}
 		});
@@ -56,6 +36,7 @@
 				console.log("Persons");
 				persons.forEach(function(p)
 				{
+					// the persons data should be entered into the HTML so the user will be able to display
 					console.log("ID: " + p.Id + " Name: " + p.Name + " Gender: " + p.Gender);
 				});
 			}
