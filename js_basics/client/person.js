753a3f44381f768f5de7df8c260ddd148b58a560
amir <amiram279@.012.net.il>
2015-02-22 19:19:44 +0200

    introduce external UI widget

--- js_basics/client/person.js
+++ js_basics/client/person.js
@@ -48,7 +48,53 @@
 												" Gender " + p.Gender +
 												"</div>");
 		});
+
+		var data = {
+			"nodes":[
+				{"id":"n1", "loaded":true, "style":{"label":"Node1"}},
+				{"id":"n2", "loaded":true, "style":{"label":"Node2"}},
+				{"id":"n3", "loaded":true, "style":{"label":"Node3"}}
+			],
+			"links":[
+				{"id":"l1","from":"n1", "to":"n2", "style":{"fillColor":"red", "toDecoration":"arrow"}},
+				{"id":"l2","from":"n2", "to":"n3", "style":{"fillColor":"green", "toDecoration":"arrow"}},
+				{"id":"l3","from":"n3", "to":"n1", "style":{"fillColor":"blue", "toDecoration":"arrow"}}
+			]
+		};
+
+		var t = new NetChart({
+			container: document.getElementById("chartContainer"),
+			height: 350,
+			data:
+			{
+				preloaded: data
+			}
+		});
 	}
+
+	var gridster = $(".gridster > ul").gridster({
+		widget_margins: [5, 5],
+		widget_base_dimensions: [100, 55]
+	}).data('gridster');
+
+	var widgets = [
+		['<li>0</li>', 1, 2],
+		['<li>1</li>', 3, 2],
+		['<li>2</li>', 3, 2],
+		['<li>3</li>', 2, 1],
+		['<li>4</li>', 4, 1],
+		['<li>5</li>', 1, 2],
+		['<li>6</li>', 2, 1],
+		['<li>7</li>', 3, 2],
+		['<li>8</li>', 1, 1],
+		['<li>9</li>', 2, 2],
+		['<li>10</li>', 1, 3]
+	];
+
+	$.each(widgets, function(i, widget){
+		gridster.add_widget.apply(gridster, widget)
+	});
+
 
 	var newPerson = {
 		Id: 2211,
